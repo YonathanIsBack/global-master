0923c543ad477b39af1dad720a55ab83148bb489
User Why <why.user.biz@gmail.com>
2026-02-10 22:40:11 +0700

    Increase paremeter limit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const server = createServer(app);
 Websocket.init(server);
 
 app.use(express.json({ limit: '200mb' }));
-app.use(express.urlencoded({ limit: '200mb', extended: true, parameterLimit: 10000 }));
+app.use(express.urlencoded({ limit: '200mb', extended: true, parameterLimit: 999999999 }));
 app.use(express.static('public'));
 app.locals.services = initServices();
 app.locals.controllers = initControllers(app.locals.services);
