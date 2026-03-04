2deb4935787572ec2605291f175b33f2e947060f
User Why <why.user.biz@gmail.com>
2026-03-04 21:21:16 +0700

    improve logger

--- src/middleware/logMiddleware.js
+++ src/middleware/logMiddleware.js
@@ -1,17 +1,17 @@
 import { DateTime } from 'luxon';
+import LogEvent from '../constants/LogEvent.js';
 import LogAPI from '../models/LogAPI.js';
 import LoggerUtilSingleton from '../util/LoggerUtils.js';
 import resDotSendInterceptor from './resDotSendInterceptor.js';
-import Constant from '../constants/Constant.js';
-import LogEvent from '../constants/LogEvent.js';
 
 const extractToken = (request) => request.get('Authorization')?.split(' ')[1] ?? '';
 
 const logMiddleware = (request, response, next) => {
   const excludedRoute = ['api-docs', '/v1/health-check'];
   response.send = resDotSendInterceptor(response, response.send);
-  const startApi = DateTime.now().setZone("Asia/Bangkok").toFormat('y-LL-dd HH:mm:ss');
-  
+  const startApi = DateTime.now().setZone('Asia/Bangkok').toFormat('y-LL-dd HH:mm:ss');
+  const startTimestamp = new Date();
+
   response.on('finish', () => {
     if (!excludedRoute.includes(request.originalUrl)) {
       const responseObject = {
@@ -24,12 +24,18 @@ const logMiddleware = (request, response, next) => {
         rtime: typeof request.body.rtime === 'undefined' ? 0 : request.body.rtime,
         authorized: response.contentBody,
         responseCode: response.statusCode,
-        cretime: DateTime.now().setZone("Asia/Bangkok").toFormat('y-LL-dd HH:mm:ss'),
+        cretime: DateTime.now().setZone('Asia/Bangkok').toFormat('y-LL-dd HH:mm:ss'),
         creby: request.body.creby_log
       };
-      const endApi = DateTime.now().setZone("Asia/Bangkok").toFormat('y-LL-dd HH:mm:ss');
+      const endApi = DateTime.now().setZone('Asia/Bangkok').toFormat('y-LL-dd HH:mm:ss');
+      const endTimestamp = new Date();
+      const apiDuration = Math.abs(endTimestamp - startTimestamp) / 1000;
       const contentType = request.headers['content-type'];
-      LoggerUtilSingleton.info(LogEvent.API, { ...responseObject, startApi, endApi, contentType });
+      LoggerUtilSingleton.info(LogEvent.API, {
+        ...responseObject,
+        timestamp: { startApi, endApi, apiDuration },
+        contentType
+      });
       LogAPI.create(responseObject);
     }
   });
