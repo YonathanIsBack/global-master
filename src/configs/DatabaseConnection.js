32163d1ae3aa0c04336d98901507eb8a0d514121
User Why <why.user.biz@gmail.com>
2026-07-15 16:38:55 +0700

    Create hook after connect

--- src/configs/DatabaseConnection.js
+++ src/configs/DatabaseConnection.js
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize';
 import getDatabaseConfig from './databaseConfig.js';
 import Constant from '../constants/Constant.js';
+import LoggerUtilSingleton from '../util/LoggerUtils.js';
 
 class DatabaseConnection {
   #connection;
@@ -23,13 +24,13 @@ class DatabaseConnection {
         logging: false,
         dialectOptions: {
           dateStrings: true,
-          typeCast: function (field, next) { 
+          typeCast: function (field, next) {
             if (field.type === 'DATETIME') {
-              return field.string()
+              return field.string();
             }
-            return next()
-          },
-        },
+            return next();
+          }
+        }
       });
       this.#connection = sequelize;
       return;
@@ -43,14 +44,34 @@ class DatabaseConnection {
       logging: true,
       dialectOptions: {
         dateStrings: true,
-        typeCast: function (field, next) { 
+        typeCast: function (field, next) {
           if (field.type === 'DATETIME') {
-            return field.string()
+            return field.string();
           }
-          return next()
-        },
+          return next();
+        }
       },
       timezone: Constant.DATABASE_TIMEZONE
+    });
+    
+    sequelize.afterConnect(async (connection) => {
+      try {
+        LoggerUtilSingleton.info('SQL', { message: 'Connected to database' });
+        console.log('after connect');
+        const [rows] = await connection.promise().query('SELECT @@SESSION.sql_mode AS mode');
+        let currentMode = rows[0].mode;
+        LoggerUtilSingleton.info('SQL', { message: `Current Mode ${currentMode}` });
+
+        let newMode = currentMode.replace('STRICT_TRANS_TABLES', '').replace(/,,+/g, ',').replace(/^,|,$/g, '');
+
+        await connection.promise().query(`SET SESSION sql_mode='${newMode}'`);
+
+        const [newRows] = await connection.promise().query('SELECT @@SESSION.sql_mode AS mode');
+        let recentMode = newRows[0].mode;
+        LoggerUtilSingleton.info('SQL', { message: `Changed Mode ${recentMode}` });
+      } catch (err) {
+        LoggerUtilSingleton.error('SQL', err);
+      }
     });
 
     this.#connection = sequelize;
